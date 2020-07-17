6216f392fbb84f3dd1f81ce5392a07e499f21798
Adam Jacobus <adam.m.jacobus@gmail.com>
2020-07-17 16:06:08 +1000

    docs: Generate initial docs

---
+++ docs/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["samd_dma"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"samd_dma/enum.TransactionError.html\" title=\"enum samd_dma::TransactionError\">TransactionError</a>","synthetic":false,"types":["samd_dma::channel::TransactionError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ docs/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["samd_dma"] = {"name":"","files":["channel.rs","descriptors.rs","lib.rs","storage.rs","types.rs"]};
+createSourceSidebar();
